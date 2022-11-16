a75ee3d213a1e61cd1d24979bdbfea5a7dc0db3d
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2022-11-16 19:53:17 -0300

    Minha resolução
    Nesta resolução fiz um pequeno complemento no código e na lógica.

--- 01 - variaveis e operadores/resolucao-desafio.js
+++ 01 - variaveis e operadores/resolucao-desafio.js
@@ -1,8 +1,15 @@
+const preço_etanol = 5.79;
+const preço_gas = 6.66;
+var tipo_combust = "Gasolina"
+const km_litro = 10;
+let dest_km = 100;
+let gastos;
 
-const precoCombustivel = 5.79;
-const kmPorLitros = 12;
-const distanciaEmKm = 100;
-
-const litrosConsumidos = distanciaEmKm / kmPorLitros;
-const valorGasto = litrosConsumidos * precoCombustivel;
-console.log(valorGasto.toFixed(2));
+console.log(tipo_combust);
+if (tipo_combust.includes("Etanol")){
+    let gastos = (dest_km/km_litro)*preço_etanol;
+    console.log("Foram gastos nessa viagem R$"+gastos.toFixed(2));
+} else {
+    tipo_combust.includes("Gasolina") ? gastos = (dest_km/km_litro)*preço_gas : "";
+    console.log("Foram gastos nessa viagem R$"+gastos.toFixed(2));
+} 
