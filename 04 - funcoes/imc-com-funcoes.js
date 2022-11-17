ac13ae458de86f8f73e8dad3912ee4b9bd1a5865
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2022-11-17 17:52:35 -0300

    My Update

--- 04 - funcoes/imc-com-funcoes.js
+++ 04 - funcoes/imc-com-funcoes.js
@@ -1,27 +1,24 @@
-
-function calcularImc(peso, altura) {
-    return peso / Math.pow(altura, 2);
+function IMC_calc(peso,altura){
+   return imc = peso / Math.pow(altura,2);
 }
 
-function classificarImc(imc) {
+function IMC_class(imc){
     if (imc < 18.5) {
-        return 'Abaixo do peso';
+        return  'Muito Abaixo do peso';
     } else if (imc >= 18.5 && imc < 25) {
-        return 'Peso normal';
+        return  'Peso normal ';
     } else if (imc >= 25 && imc < 30) {
-        return 'Acima do peso';
+        return 'Acima do peso' ;
     } else if (imc >= 30 && imc < 40) {
         return 'Obeso';
     } else {
-        return 'Obsedidade Grave';
-    }
+        return 'Obsedidade Grave' ;}
 }
 
-// Main
+
 (function () {
-    const peso = 75;
-    const altura = 1.75;
-    
-    const imc = calcularImc(peso, altura);
-    console.log(classificarImc(imc));
+    const peso = 80;
+    const altura = 1.83;
+    var imc = IMC_calc(peso,altura) ;
+    console. log(IMC_class(imc));
 })();
